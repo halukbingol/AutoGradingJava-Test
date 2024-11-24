a35b1340e7eb7182ef287e9d49e79659c7395463
Haluk Bingol <haluk.bu@gmail.com>
2024-11-24 17:08:16 +0300

    init

---
+++ AutoGradingJava-Test-Solution/src/grading_2099_3_midterm_1/pack_a/Adder_Test.java
@@ -0,0 +1,9 @@
+package grading_2099_3_midterm_1.pack_a;
+
+public class Adder_Test {
+
+	public static void main(String[] args) {
+		System.out.println(Adder.add(6, 7));
+	}
+
+}
---
+++ AutoGradingJava-Test-Solution/src/grading_2099_3_midterm_1/pack_b/ProductOfSums.java
@@ -0,0 +1,25 @@
+package grading_2099_3_midterm_1.pack_b;
+
+import grading_2099_3_midterm_1.pack_a.Adder;
+import grading_2099_3_midterm_1.pack_a.Multiplier;
+
+public class ProductOfSums {
+
+	public static int multiplyAdd(int a, int b, int c, int d) {
+		// below-0123456789-V toDo  // do not change this line ~~~~~~~~~~ V
+		// TODO your code should be between below and above marks.
+
+		// below-0123456789-9876543210-V solution >>>>>>>>> V
+		// ~~fake~~ return 0; // fake
+
+		int x = Adder.add(a, b);
+		int y = Adder.add(c, d);
+		int z = Multiplier.multiply(x, y);
+		return z;
+
+		// above-0123456789-9876543210-A solution <<<<<<<<< A
+
+		// above-0123456789-A toDo  // do not change this line ~~~~~~~~~~ A
+	}
+	
+}
