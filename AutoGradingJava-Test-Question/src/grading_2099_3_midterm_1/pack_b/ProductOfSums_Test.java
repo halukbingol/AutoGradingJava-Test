82147919bb886a88469361a37d9ee44161246df9
Haluk Bingol <haluk.bu@gmail.com>
2024-11-24 17:29:16 +0300

    AutoGradingJava-Test-Question is added

---
+++ AutoGradingJava-Test-Question/src/grading_2099_3_midterm_1/pack_b/ProductOfSums_Test.java
@@ -0,0 +1,9 @@
+package grading_2099_3_midterm_1.pack_b;
+
+public class ProductOfSums_Test {
+
+	public static void main(String[] args) {
+		System.out.println(ProductOfSums.multiplyAdd(5, 6, 7, 8));
+	}
+
+}
